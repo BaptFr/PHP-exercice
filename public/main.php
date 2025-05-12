33c8197ffd2026c5780fa563fd8ffc382f1a953c
BaptFR <baptistesalazar@gmail.com>
2025-05-12 15:28:18 +0200

    Class début exercice

--- public/main.php
+++ public/main.php
@@ -1,29 +1,34 @@
 <?php
-
-
 echo 'TEST  DOCKER affichage page';
 
-
+//représente résultat de tirage aléatoire
 class Resultat_tirage {
-    public function __construct()
-    {
-        echo 'Tirage aléatoire';
-    }
-}
 
-class Valeur_tirage{
-    public function __construct()
-    {
-        echo 'Valeur de tirage';
+   public function get_resultat() {
+        return $this->resultat;
     }
+    public function set_resultat($resultat) {
+        $this->resultat = $resultat;
+    }
+};
+
+//Tirage valeurs aléatoires
+class Comportement_tirage{
+
+};
+
+class Dé {
+
+
 }
-class Dé
-{
-    public function __construct()
-    {
-        echo 'Dé';
-    }
-} 
+class Pièce {
+
+    
+}
+class Deck {
+
+    
+}
 ?>
 
 
