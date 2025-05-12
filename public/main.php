0975efcd1f86b96cb0a8b396c15fb96ed8bdae0d
BaptFR <baptistesalazar@gmail.com>
2025-05-12 14:29:39 +0200

    first commit

---
+++ public/main.php
@@ -0,0 +1,30 @@
+<?php
+
+
+echo 'TEST  DOCKER affichage page';
+
+
+class Resultat_tirage {
+    public function __construct()
+    {
+        echo 'Tirage aléatoire';
+    }
+}
+
+class Valeur_tirage{
+    public function __construct()
+    {
+        echo 'Valeur de tirage';
+    }
+}
+class Dé
+{
+    public function __construct()
+    {
+        echo 'Dé';
+    }
+} 
+?>
+
+
+ 
