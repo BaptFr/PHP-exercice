79e2413b0824ed396e1e3bb39fd0594f956fc7f2
BaptFR <baptistesalazar@gmail.com>
2025-05-13 15:34:57 +0200

    ex suite

--- public/main.php
+++ public/main.php
@@ -5,67 +5,73 @@ use LDAP\Result;
 echo 'TEST  DOCKER affichage page';
 
 
-
-
-
 class GameMaster{
  //choix aléatoire d'un matériel pour tirage
 }
 
-
-interface Comportement_tirage{
-    //fonction tirage  :resultat.
-    public function tirer(): Resultat_tirage;
- 
-
-};
-
-
-//résultat tirage aléatoire par GM
 class Resultat_tirage {
     public string $type;
-    public string $resultat;
-    //variable valeur ???
+     public string $resultat;
+     //variable valeur??
 
      public function __construct(string $type, string $resultat ) //Valeur
-        {
-        $acceptedTypes = ['Dé', 'Pièce', 'Deck'];
-        $acceptedResultat = ['Réussite', 'Echec', 'Réussite critique', 'Fumble'];  
-            if (!in_array($this ->$type, $acceptedTypes)) {
-                throw new \Exception("Type de tirage invalide");
-            }  
-            if (!in_array($this ->$resultat, $acceptedResultat)) {
-                throw new \Exception("Résultat du tirage invlide");
-            }
+        { 
         }
 };
 
+interface Tirage_aleatoire {
+    public function tirer(): Resultat_tirage;
+}
+
 //Matériels utilisés, interface comportement tirage
-// CHaque mat renvoi Resultat_tirage
-class Dé implements Comportement_tirage {
-    public function lancer() : Resultat_tirage {
-        $chiffre = (1;6);
-        $resultat_lancé = $chiffre;
+// Chaque matériel renvoi Resultat_tirage
+// class Dé implements Comportement_tirage {
+//     public function tirer() : Resultat_tirage {
+//         $chiffre = rand(1;6);
+//         $resultat_lancé = $chiffre;
+//     }
+// }
+
+
+class Dé {
+    public int $value;
+    public function __construct(
+        public readonly int $nbFaces 
+        ){
+            $this->nbFaces = $nbFaces;
+            $this->roll();
+        }
+    public function roll () {
+        $this->value = rand(1, $this->nbFaces);
+    }
+
+    public function setValue(int $value) {
+        $this->value = $value;
     }
 }
 
-class Pièce implements Comportement_tirage{
-    public function jeter() : Resultat_tirage {
-        $face = (p;f);
-        $resultat_tirage = $face ;
+
+class Pièce {
+    public int $value;
+    public function __construct(
+    $nbLancers = 1 ) {
+        $this->flip();
+    }
+    
+    public function flip() {
+        $this->value = rand (0, 1);
     }
 }
-class Deck implements Comportement_tirage{
-    public function tirer_carte() : Resultat_tirage{
-        $carte_tirée= (1;52);
-        $resultat_tirage = $carte_tirée;
-    }
 
+class Deck {}
+
+enum Resultat : string
+{
+    case FUMBLE = 'fumble';
+    case ECHEC = 'échec';
+    case REUSSITE = 'réussite';
+    case REUSSITE_CRITIQUE = 'réussite critique';
 }
-
-
-
-
 
 
 ?>
